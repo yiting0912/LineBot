f3cab6fce6cfd77b8924b4e1e907a0680c8c30b1
yiting0912 <yuyiting.youeating@gmail.com>
2022-02-10 15:31:04 +0800

    updateImageMessage

--- app.py
+++ app.py
@@ -239,8 +239,8 @@ def getTaipei101ImageMessage(originalContentUrl=F"{end_point}/static/taipei_101.
 def getImageMessage(originalContentUrl):
     message = dict()
     message["type"] = "image"
-    message["originalContentUrl"]: originalContentUrl
-    message["previewImageUrl"]: originalContentUrl
+    message["originalContentUrl"] = originalContentUrl
+    message["previewImageUrl"] = originalContentUrl
     return message
 
 
