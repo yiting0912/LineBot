4ddf288ce741324862e9b3213c56a9ec3c2b4464
yiting0912 <yuyiting.youeating@gmail.com>
2022-02-10 18:48:13 +0800

    updateLocationConfirmMessage

--- app.py
+++ app.py
@@ -197,7 +197,26 @@ def getCarouselMessage(data):
 
 
 def getLocationConfirmMessage(title, latitude, longitude):
-    message = dict()
+    message = {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text": "Are you sure?",
+          "actions": [
+              {
+                "type": "message",
+                "label": "Yes",
+                "text": "yes"
+              },
+              {
+                "type": "message",
+                "label": "No",
+                "text": "no"
+              }
+          ]
+      }
+    }
     return message
 
 
